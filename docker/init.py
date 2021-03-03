75aa8c85b3021b5df6c14fbfbc18d52c7d88aaac
Adam Duskett <aduskett@rivian.com>
2021-03-03 13:49:31 -0800

    Better env.json parsing

--- docker/init.py
+++ docker/init.py
@@ -1,13 +1,55 @@
 #!/usr/bin/env python3
+import os
+import sys
+import argparse
+import signal
 from lib.init_parse import InitParse
 
 
-def main():
-    init = InitParse("env.json")
-    init.clean_all()
-    init.apply_configs()
-    init.update_buildroot()
-    init.build_all()
+def parse_args(args) -> argparse.Namespace:
+    """Parse arguments.
+
+    param args: an argument array
+    :return: The argument object.
+    :rtype: object
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-e",
+        "--env-files",
+        type=str,
+        default="env.json",
+        help="A colon-eliminated list of environment files",
+    )
+
+    return parser.parse_args(args)
+
+
+def signal_handler(sig, _):
+    """Handle signals.
+
+    :param int sig: The signal of which to handle.
+    :param object _: unused frame stack.
+    """
+    if sig == signal.SIGINT:
+        print("\n## Exiting. ##\n")
+        sys.exit(0)
+
+
+def main(args=None):
+    cwd = os.getcwd()
+    signal.signal(signal.SIGINT, signal_handler)
+    args = parse_args(args)
+    env_files = args.env_files.split(":")
+    for env_file in env_files:
+        if not os.path.isfile(env_file):
+            print("{}: No such file".format(env_file))
+            exit(1)
+    for env_file in env_files:
+        init = InitParse(env_file)
+        init.run()
+        os.chdir(cwd)
 
 
 if __name__ == "__main__":
--- docker/lib/init_parse.py
+++ docker/lib/init_parse.py
@@ -3,8 +3,7 @@ import os
 import sys
 import json
 import logging
-from shutil import move
-import tempfile
+import subprocess
 from typing import Any, List, Dict, Tuple, Union
 from lib.dirs import Dirs
 from lib.files import Files
@@ -50,17 +49,33 @@ class InitParse:
                 str(type(config[attribute])).split("'")[1],
             )
             sys.exit(1)
-        if fail:
-            logging.error("%s attribute missing in env.json", attribute)
+        elif fail:
+            print("Mandatory attribute: {} not defined!".format(attribute))
             sys.exit(1)
-        if default_value:
+        if default_value or isinstance(default_value, bool):
             return True, default_value
         return retval, attribute_val
 
-    def __parse_paths(self, config) -> None:
+    def __parse_paths(self, config: Dict[str, Any]) -> None:
+        """Parse all paths from a given config."""
         defconfig = str(config["defconfig"])
+        output_tree = self.__parse_config_attr(config, "output_tree", str, fail=True)[1]
+        output_dir_base = "{}/{}".format(self.buildroot_path, output_tree)
+        output_dir_name = self.__parse_config_attr(
+            config,
+            "output_dir_name",
+            str,
+            "output".format(output_dir_base),
+        )[1]
+        self.output_dir = "{}/{}".format(output_dir_base, output_dir_name)
+        config_dir_name = self.__parse_config_attr(
+            config, "config_dir_name", str, default_value="configs"
+        )[1]
+        self.config_dir_path = "{}/{}/{}".format(
+            self.buildroot_path, self.config_dir_tree, config_dir_name
+        )
         self.defconfig = defconfig.replace("_defconfig", "") + "_defconfig"
-        self.defconfig_path = "{}/{}".format(self.config_dir, self.defconfig)
+        self.defconfig_path = "{}/{}".format(self.config_dir_path, self.defconfig)
 
         if self.__parse_config_attr(config, "name", str)[0]:
             self.build_path = "{}/{}".format(self.output_dir, config["name"])
@@ -74,47 +89,79 @@ class InitParse:
             sys.exit(1)
 
     def __parse_fragments(self, config: Dict[str, Any]) -> None:
-        extern_tree_base = "{}/{}".format(self.buildroot_path, self.external_tree_name)
-        if self.__parse_config_attr(config, "fragments", list)[0]:
-            fragments = list(config["fragments"])
-            retval = self.__parse_config_attr(config, "fragment_dir", str)
-            if retval[0]:
-                self.fragment_dir = "{}/{}".format(extern_tree_base, retval[1])
-            else:
-                self.fragment_dir = "{}/configs".format(self.config_dir)
-            for fragment in fragments:
-                fragment_path = "{}/{}".format(self.fragment_dir, fragment)
-                Files.exists(fragment_path, fail=True)
-                self.fragments.append(fragment_path)
-        else:
-            self.fragments.clear()
+        """Parse all fragments if available."""
+        ndx = 0
+        fragments_temp: List[str] = []
+        self.fragments.clear()
+        for external_tree in self.external_trees.split(":"):
+            extern_tree_base = "{}/{}".format(self.buildroot_path, external_tree)
+            extern_tree = config["external_trees"][ndx]
+            ndx += 1
+            if self.__parse_config_attr(extern_tree, "fragments", list)[0]:
+                fragments = list(extern_tree["fragments"])
+                retval = self.__parse_config_attr(extern_tree, "fragment_dir", str)
+                if retval[0]:
+                    self.fragment_dir = "{}/{}".format(extern_tree_base, retval[1])
+                else:
+                    self.fragment_dir = "{}/configs".format(self.config_dir_path)
+                for fragment in fragments:
+                    fragment_path = "{}/{}".format(self.fragment_dir, fragment)
+                    Files.exists(fragment_path, fail=True)
+                    fragments_temp.append(fragment_path)
+        self.fragments = fragments_temp
+
+    def __parse_external_trees(self, config: Dict[str, Any]) -> str:
+        external_tree_string: str = ""
+        external_tree: Dict[str, Any]
+        self.external_trees = ""
+        self.__parse_config_attr(config, "external_trees", list, fail=True)
+        external_trees: List[str] = config["external_trees"]
+
+        for external_tree in external_trees:
+            external_tree_string += (
+                self.__parse_config_attr(external_tree, "name")[1] + ":"
+            )
+        return external_tree_string[:-1]
 
     def _parse_config_settings(self, config: Dict[str, Union[str, bool]]) -> None:
         self.make = "make"
         self.__parse_config_attr(config, "defconfig", str, fail=True)
-        self.build = self.__parse_config_attr(config, "build", bool, False)[1]
+        self.build = self.__parse_config_attr(config, "build", bool, True)[1]
+        self.skip = self.__parse_config_attr(config, "skip", bool, False)[1]
         self.clean = self.__parse_config_attr(config, "clean", bool, False)[1]
         self.remove = self.__parse_config_attr(config, "remove", bool, False)[1]
-        if self.__parse_config_attr(config, "verbose", bool, False)[0]:
+        if not self.__parse_config_attr(config, "verbose", bool, False)[1]:
             self.make = "brmake"
+        self.external_trees = self.__parse_external_trees(config)
+        self.config_dir_tree = self.__parse_config_attr(
+            config, "config_dir_tree", str, fail=True
+        )[1]
 
-        self.external_tree_name = self.__parse_config_attr(
-            config, "external_tree_name", str, fail=True
-        )[1]
-        self.output_dir = "{}/{}/{}".format(
-            self.buildroot_path,
-            self.external_tree_name,
-            self.__parse_config_attr(
-                config, "output_dir", str, "{}/output".format(self.buildroot_path)
-            )[1],
-        )
-        extern_tree_base = "{}/{}".format(self.buildroot_path, self.external_tree_name)
-        config_dir = self.__parse_config_attr(
-            config, "config_dir", str, default_value="configs"
-        )[1]
-        self.config_dir = "{}/{}".format(extern_tree_base, config_dir)
         self.__parse_paths(config)
         self.__parse_fragments(config)
+
+    def __apply_fragments(self) -> None:
+        """Apply the config fragments defined in env.json for a given config.
+
+        Note: We run "make olddefconfig" to force the kconfig system to rebuild the .config
+              with the default dependencies selected for the added fragments.
+        """
+        for fragment in self.fragments:
+            self.__print_step("Applying fragment: {}".format(fragment))
+            fragment_buff = Files.to_buffer(file_location=fragment)
+            Files.save_buffer(
+                "{}/.config".format(self.build_path),
+                fragment_buff,
+                append=True,
+            )
+        os.chdir(self.build_path)
+        with open(os.devnull, "wb") as null:
+            subprocess.check_call(
+                [self.make, "olddefconfig"],
+                stdout=null,
+                stderr=subprocess.STDOUT,
+            )
+        os.chdir(self.buildroot_path)
 
     def apply_configs(self) -> None:
         """Apply all configs defined in env.json."""
@@ -127,35 +174,13 @@ class InitParse:
                 "BR2_DL_DIR", self.defconfig_path, self.buildroot_path
             )
             if not dl_dir:
-                dl_dir = "{}/{}/dl".format(self.buildroot_path, self.external_tree_name)
+                dl_dir = "{}/{}/dl".format(self.buildroot_path, self.external_trees)
             Dirs.exists(dl_dir, make=True, fail=True)
 
             if not Dirs.exists(self.build_path):
-                if self.fragments:
-                    tmp_defconfig, tmp_defconfig_path = tempfile.mkstemp(dir="/tmp")
-                    defconfig_buff = Files.to_buffer(self.defconfig_path)
-                    Files.save_buffer(
-                        tmp_defconfig_path, defconfig_buff, overwrite=True
-                    )
-                    for fragment in self.fragments:
-                        fragment_buff = Files.to_buffer(file_location=fragment)
-                        Files.save_buffer(
-                            tmp_defconfig_path,
-                            fragment_buff,
-                            append=True,
-                        )
-                    os.close(tmp_defconfig)
-                    new_defconfig_name = "{}_defconfig".format(
-                        os.path.basename(tmp_defconfig_path)
-                    )
-                    new_defconfig_path = "{}/configs/{}".format(
-                        self.buildroot_path, new_defconfig_name
-                    )
-                    move(tmp_defconfig_path, new_defconfig_path)
-                    self.defconfig = new_defconfig_name
                 cmd = "BR2_EXTERNAL={}/{} BR2_DL_DIR={} BR2_DEFCONFIG={} {} {} O={}".format(
                     self.buildroot_path,
-                    self.external_tree_name,
+                    self.external_trees,
                     dl_dir,
                     self.defconfig_path,
                     self.make,
@@ -166,14 +191,7 @@ class InitParse:
                 self.__print_step("Applying {}".format(self.defconfig_path))
                 os.system(cmd)
                 if self.fragments:
-                    os.system(
-                        'sed s%BR2_DEFCONFIG=.*%BR2_DEFCONFIG=\\"{}\\"%g -i {}'.format(
-                            self.defconfig_path, self.build_path + "/.config"
-                        )
-                    )
-                    os.remove(
-                        "{}/configs/{}".format(self.buildroot_path, self.defconfig)
-                    )
+                    self.__apply_fragments()
 
     def clean_all(self) -> None:
         """Clean all configs that have the clean boolean set to true."""
@@ -189,6 +207,7 @@ class InitParse:
                     cmd = "{} clean".format(self.make)
                     self.__print_step("Cleaning {}".format(self.defconfig))
                     os.system(cmd)
+                    os.chdir(self.buildroot_path)
 
     def update_buildroot(self) -> Union[bool, int]:
         """Update buildroot if set to true."""
@@ -197,20 +216,23 @@ class InitParse:
         return True
 
     def _parse_env(self):
-        self.user = "br-user" if "user" not in self.env else self.env["user"]
-        self.update = (
-            False
-            if "update_buildroot" not in self.env
-            else self.env["update_buildroot"]
-        )
-        self.buildroot_path = (
-            "/home/" + self.user + "/buildroot"
-            if "buildroot_dir" not in self.env
-            else "/home/" + self.user + "/" + self.env["buildroot_dir"]
-        )
-        self.exit = (
-            True if "exit_after_build" not in self.env else self.env["exit_after_build"]
-        )
+        if "environment" in self.env:
+            environment = self.env["environment"][0]
+        else:
+            environment = {}
+        self.exit = self.__parse_config_attr(
+            environment, "exit_after_build", bool, True
+        )[1]
+        self.update = self.__parse_config_attr(
+            environment, "update_buildroot", bool, False
+        )[1]
+        self.user = self.__parse_config_attr(environment, "user", str, "br-user")[1]
+        self.buildroot_path = self.__parse_config_attr(
+            environment,
+            "buildroot_dir_name",
+            str,
+            "/home/{}/buildroot".format(self.user),
+        )[1]
 
     def build_all(self):
         """Build all configs that have the build attribute set to true in env.json"""
@@ -223,25 +245,40 @@ class InitParse:
             else:
                 self.__print_step("{}: Skip build step".format(self.defconfig))
 
-    def __init__(self, env_file):
+    def run(self) -> None:
+        """Run all the steps."""
+        self._parse_env()
+        self.buildroot_path: str = "/home/{}/buildroot".format(self.user)
+        for config in self.env["configs"]:
+            self._parse_config_settings(config)
+            if self.skip:
+                self.__print_step("Skipping {}".format(self.defconfig))
+                continue
+            self.clean_all()
+            self.apply_configs()
+            self.update_buildroot()
+            self.build_all()
+
+    def __init__(self, env_file: str):
         with open(env_file) as env_fd:
             self.env = json.load(env_fd)
-        self._parse_env()
         self.build: bool = False
         self.build_path: str = ""
-        self.buildroot_path: str = "/home/{}/buildroot".format(self.user)
+        self.buildroot_path: str = "/home/br-user/buildroot"
         self.clean: bool = False
-        self.config_dir: str = "configs"
+        self.config_dir_path: str = "configs"
+        self.config_dir_tree: str = ""
         self.defconfig: str = ""
         self.defconfig_path: str = ""
         self.exit: bool = False
-        self.external_tree_name: str = ""
+        self.external_trees: str = ""
         self.fragment_dir: str = ""
         self.fragments: List[str] = []
         self.make: str = "make"
         self.output_name: str = ""
         self.output_dir: str = ""
         self.remove: bool = False
+        self.skip: bool = False
         self.update: bool = False
         self.user: str = "br-user"
         self.verbose: bool = False
